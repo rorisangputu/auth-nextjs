abaa970eb419c5a742edde4f41fbceef7b95ef58
Rorisang Putu <rorisangputu@gmail.com>
2025-06-09 18:48:37 +0200

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Page = async () => {
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
         <p className="text-gray-600">Signed in as:</p>
-        <p className="font-medium">TODO</p>
+        <p className="font-medium">TODO App</p>
       </div>
 
       <SignOut />

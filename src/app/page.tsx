d53618969f54eb36148957477f8e25612997c4b3
Rorisang19 <rorisangputu@gmail.com>
2025-06-10 09:16:55 +0200

    feat: add user session handling and redirect to sign-in if not authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,14 @@ const Page = async () => {
   const user = session.user;
   return (
     <>
-      <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
+      <div className="bg-gray-100 rounded-lg p-4 flex flex-col items-center text-center mb-6">
         <p className="text-gray-600">Signed in as:</p>
         <p className="font-medium">{user?.name}</p>
+        {user?.image && 
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={user?.image as string} alt={user?.name as string} 
+            className="h-32 w-32 m-5 rounded-full"
+          />}
       </div>
 
       <SignOut />

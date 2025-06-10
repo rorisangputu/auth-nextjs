172191bd709bc93fea33e7e94cda1ee148178b22
Rorisang19 <rorisangputu@gmail.com>
2025-06-10 09:38:37 +0200

    feat: add credentials provider for user authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,25 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 
-export const { auth, handlers, signIn,  } = NextAuth({ providers: [GitHub] });
+export const { auth, handlers, signIn,  } = NextAuth({ 
+    providers: [
+        GitHub, 
+        Credentials({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                const email= "admin@gmail.com";
+                const password = "12345";
+
+                if(credentials.email === email && credentials.password === password){
+                    return { email, password };
+                } else {
+                    throw new Error("Invalid credentials")
+                }
+            },
+        }),
+    ],
+});

a865d50648a74c050b5ca1db0ccd1dfbab5e91b5
Rorisang Putu <rorisangputu@gmail.com>
2025-06-10 20:34:57 +0200

    refactor: improve structure and readability of auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,33 +4,65 @@ import GitHub from "next-auth/providers/github";
 import db from "./db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { schema } from "./schema";
+import { v4 as uuid } from "uuid";
+import { encode } from "next-auth/jwt";
 
 const adapter = PrismaAdapter(db);
 
-export const { auth, handlers, signIn,  } = NextAuth({ 
-    adapter: adapter,
-    providers: [
-        GitHub, 
-        Credentials({
-            credentials: {
-                email: {},
-                password: {},
-            },
-            authorize: async (credentials) => {
-                
-                const validatedCredentials = schema.parse(credentials)
-                const user = await db.user.findFirst({
-                    where: {
-                        email: validatedCredentials.email,
-                        password: validatedCredentials.password
-                    }
-                });
+export const { auth, handlers, signIn } = NextAuth({
+  adapter: adapter,
+  providers: [
+    GitHub,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const validatedCredentials = schema.parse(credentials);
+        const user = await db.user.findFirst({
+          where: {
+            email: validatedCredentials.email,
+            password: validatedCredentials.password,
+          },
+        });
 
-                if(!user){
-                    throw new Error("Invalid credentials")
-                }
-                return user;
-            },
-        }),
-    ],
+        if (!user) {
+          throw new Error("Invalid credentials");
+        }
+        return user;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.provider === "credentials") {
+        token.credentials = true;
+      }
+      return token;
+    },
+  },
+  jwt: {
+    encode: async function (params) {
+      if (params.token?.credentials) {
+        const sessionToken = uuid();
+
+        if (!params.token.sub) {
+          throw new Error("No user ID found in token");
+        }
+
+        const createdSession = await adapter?.createSession?.({
+          sessionToken: sessionToken,
+          userId: params.token.sub,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        });
+        if (!createdSession) {
+          throw new Error("Failed to create session");
+        }
+        return sessionToken;
+      }
+      return encode(params);
+    },
+  },
 });

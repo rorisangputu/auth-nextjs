25726145b01d5c509e39b7f3898aa6775465c792
Rorisang Putu <rorisangputu@gmail.com>
2025-06-10 20:34:44 +0200

    fix: correct formatting and structure of user image rendering in Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,15 @@ const Page = async () => {
       <div className="bg-gray-100 rounded-lg p-4 flex flex-col items-center text-center mb-6">
         <p className="text-gray-600">Signed in as:</p>
         <p className="font-medium">{user?.name || user?.email}</p>
-        {user?.image && 
+        <p></p>
+        {user?.image && (
           // eslint-disable-next-line @next/next/no-img-element
-          <img src={user?.image as string} alt={user?.name as string} 
+          <img
+            src={user?.image as string}
+            alt={user?.name as string}
             className="h-32 w-32 m-5 rounded-full"
-          />}
+          />
+        )}
       </div>
 
       <SignOut />
